8ef413e33fcdceb29c3f361b4c5d2896850b793a
Paolo Gallitelli <paologallitelli05@gmail.com>
2023-05-09 12:52:36 +0200

    exe12

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return <div>
-          <Welcome name = "Paolo" age = {40}/>
+          <Welcome name = "John" age = {16}/>
         </div>
 }
 
--- myapp/src/components/Welcome.js
+++ myapp/src/components/Welcome.js
@@ -1,14 +1,13 @@
 import Age from "./Age"
 
 function Welcome(props){
-    return <div>
-                <p>Welcome {props.name}!</p>
-                {props.age >= 18 && props.age <= 65
-                ? <p>Your age is {props.age}</p>
-                : <p>Insert a valid age, between 18 and 65</p>
-                }
+    return <>
+                <p>Welcome {props.name}!</p> 
+                {props.name === "John" && props.age >= 18 && props.age <= 65
+                ? <p>Your age is {props.age}</p> 
+                : <p>Your name is not John or you're not between 18 and 65</p>}
                 <Age/>
-            </div>
+                </>
 }
 
 export default Welcome
