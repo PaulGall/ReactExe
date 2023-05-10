9ba151f71784da7dc8e9dbf0e0907ac36e0bcd52
Paolo Gallitelli <paologallitelli05@gmail.com>
2023-05-10 13:42:27 +0200

    exe26

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,14 @@
-import TodoList from './components/TodoList';
-import './App.css';
+import TodoList from "./components/TodoList";
+import "./App.css";
 
 function App() {
+  const arrNames = ["Paolo", "Margot", "Alessandro"];
 
-  const arrNames = ['Paolo', 'Margot', 'Alessandro'];
-
-  return <div>
-          <TodoList name = {arrNames}/>
-        </div>
+  return (
+    <div>
+      <TodoList name={arrNames} />
+    </div>
+  );
 }
 
 export default App;
--- myapp/src/components/TodoList.jsx
+++ myapp/src/components/TodoList.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const TodoList = (props) => {
   const [name, setName] = useState("");
   const [item, setItem] = useState(props.name);
+  let clearInput = useRef("");
 
   const setNameHandler = () => {
     setItem([...item, name]);
-    setName("");
+    clearInput.current.value = null;
   };
 
   return (
     <>
       <h3>Todo List</h3>
-      <input onChange={(event) => setName(event.target.value)} />
+      <input
+        onChange={(event) => setName(event.target.value)}
+        ref={clearInput}
+      />
       <button type="button" onClick={setNameHandler}>
         Add Item
       </button>
