689c56ec40f8f84b35ff0dbceff0464e2942c740
Paolo Gallitelli <paologallitelli05@gmail.com>
2023-05-10 13:20:31 +0200

    exe22

--- myapp/src/components/Form.jsx
+++ myapp/src/components/Form.jsx
@@ -1,24 +1,38 @@
 import { useState } from "react";
+import { useRef } from "react";
 import Login from "./Login"
 
 const Form = () => {
 
   const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [pass, setPass] = useState("");
 
   const setNameHandler = (event) => {
     setName({ name: event.target.value });
   };
 
+  const setLastNameHandler = (event) => {
+    setLastName({lastName: event.target.value})
+  }
+
   const setPassHandler = (event) => {
     setPass({ pass: event.target.value });
   };
 
+  const emptyName = useRef('');
+  const emptyLastName = useRef('');
+  const emptyPass = useRef('');
+
   return (
     <form>
-      <input type="text" onChange={setNameHandler} />
-      <input type="password" onChange={setPassHandler} />
-      <Login text={name} pass={pass} />
+      <input type="text" onChange={setNameHandler}  ref = {emptyName}/>
+      <br />
+      <input type="text" onChange={setLastNameHandler} ref = {emptyLastName}/>
+      <br />
+      <input type="password" onChange={setPassHandler} ref = {emptyPass}/>
+      <br />
+      <Login name={name} lastName={lastName} pass={pass} emptyName={emptyName} emptyLastName = {emptyLastName} emptyPass = {emptyPass}/>
     </form>
   );
 }
--- myapp/src/components/Login.jsx
+++ myapp/src/components/Login.jsx
@@ -1,17 +1,27 @@
-
 const Login = (props) => {
 
     const onLogin = (event) => {
         event.preventDefault();
     let values = {
-        name: props.text,
+        name: props.name,
+        lastName: props.lastName,
         password: props.pass,
     }
     console.log(values)
     }
+
+    const resetContent = (event) => {
+        event.preventDefault();
+        props.emptyName.current.value = '';
+        props.emptyLastName.current.value = '';
+        props.emptyPass.current.value = '';
+    }
+
+    
         return(
             <>
-                <button disabled={!props.text || !props.pass} onClick={onLogin}>Login</button>
+                <button disabled={!props.name || !props.pass ||!props.lastName} onClick={onLogin}>Login</button>
+                <button onClick={resetContent}>Reset Content</button>
             </>
         )
 }
