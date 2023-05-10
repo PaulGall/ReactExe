30b24a44f8452df5e9daf443079a9cff818d14ba
Paolo Gallitelli <paologallitelli05@gmail.com>
2023-05-10 13:55:17 +0200

    exe27

--- myapp/src/components/TodoList.jsx
+++ myapp/src/components/TodoList.jsx
@@ -3,11 +3,18 @@ import { useState, useRef } from "react";
 const TodoList = (props) => {
   const [name, setName] = useState("");
   const [item, setItem] = useState(props.name);
+  const [reset, setReset] = useState("");
   let clearInput = useRef("");
+  let clearList = useRef("");
 
   const setNameHandler = () => {
     setItem([...item, name]);
     clearInput.current.value = null;
+  };
+
+  const setResetHandler = () => {
+    setItem([...reset]);
+    clearList.current = [];
   };
 
   return (
@@ -20,7 +27,10 @@ const TodoList = (props) => {
       <button type="button" onClick={setNameHandler}>
         Add Item
       </button>
-      <ul>
+      <button type="button" onClick={setResetHandler}>
+        Reset List
+      </button>
+      <ul ref={clearList} onChange={(event) => setReset(event.target.value)}>
         {item.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
