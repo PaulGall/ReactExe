060c0b18dece9216e5b58ef0215cd1686e7135e1
Paolo Gallitelli <paologallitelli05@gmail.com>
2023-05-20 12:12:53 +0200

    exe47

---
+++ myapp/src/App.jsx
@@ -0,0 +1,15 @@
+import { Link, Route, Routes } from "react-router-dom";
+import ShowGithubUser from "./components/ShowGithubUser";
+
+function App() {
+  return (
+    <div>
+      <Link to="/user/Paul">Go to User</Link>
+      <Routes>
+        <Route path="user/:username" element={<ShowGithubUser />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/Root.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+function Root() {
+  return (
+    <>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default Root;
