31aa77294631f4437b340c6d192545b270059ac0
Paolo Gallitelli <paologallitelli05@gmail.com>
2023-05-20 12:18:53 +0200

    exe48

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,15 +1,18 @@
 import { Link, Route, Routes } from "react-router-dom";
 import ShowGithubUser from "./components/ShowGithubUser";
 
+
 function App() {
-  return (
-    <div>
-      <Link to="/user/Paul">Go to User</Link>
-      <Routes>
-        <Route path="user/:username" element={<ShowGithubUser />} />
-      </Routes>
-    </div>
-  );
+  return <div>
+          <Routes>
+            <Route path="user/:username" element={<ShowGithubUser/>}/>
+          </Routes>
+          <Link to='/user/Rushizo'>Go to User Danilo Carbone</Link>
+          <br />
+          <Link to='/user/yangus98'>Go to User Giuseppe Bianconi</Link>
+          <br />
+          <Link to='/user/RobertoFanara'>Go to User Roberto Fanara</Link>
+        </div>
 }
 
 export default App;
